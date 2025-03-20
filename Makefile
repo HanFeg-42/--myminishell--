f80778ff2d1f06f966c7041eb0eda2217f437a0a
HanFeg-42 <hananefegrachi2002@gmail.com>
2025-03-20 11:19:48 +0000

    figuring out hoe to deal wo milk

---
+++ Makefile
@@ -0,0 +1,24 @@
+NAME = minishell
+
+CC = cc
+
+RM = rm -f
+
+CFLAGS = -Wall -Wextra -Werror
+
+RLFLAG = -lreadline
+
+SRC = src/parsing/intro_to_shell/readline.c
+
+OBJ = ${SRC:.c=.o}
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) $(RLFLAG) -o $(NAME)
+
+clean:
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(NAME)
--- src/parsing/intro_to_shell/readline.c
+++ src/parsing/intro_to_shell/readline.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
+void handler(int sig)
+{
+    (void)sig;
+    rl_on_new_line();
+    // printf("^C");
+}
 
 int main()
 {
     char *line;
 
+    signal(SIGINT, handler);
     while (1)
     {
         line = readline("\033[38;2;255;105;180mMinishell > \033[0m");
+        if (!line)
+            break;
         printf("This is what you wrote:: %s\n", line);
-        
+        add_history(line);
         free(line);
     }
     return (0);
