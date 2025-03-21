8ee4eb513e59c8f460b235d14e7daa9e7019557f
HanFeg-42 <hananefegrachi@gmail.com>
2025-03-21 11:07:28 +0100

    handle Ctrl + C

--- src/parsing/intro_to_shell/readline.c
+++ src/parsing/intro_to_shell/readline.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -7,8 +8,10 @@
 void handler(int sig)
 {
     (void)sig;
+    printf("\n");
+    // rl_replace_line("", 0); // hadi kima glt ma3ndha tachi dawr flhayat
     rl_on_new_line();
-    // printf("^C");
+    rl_redisplay();
 }
 
 int main()
@@ -19,7 +22,7 @@ int main()
     while (1)
     {
         line = readline("\033[38;2;255;105;180mMinishell > \033[0m");
-        if (!line)
+        if (!line || !strcmp(line, "exit"))
             break;
         printf("This is what you wrote:: %s\n", line);
         add_history(line);
