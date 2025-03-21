0fcd4622bc138ba712b9c3e155c8a2d0193bb3c3
HanFeg-42 <hananefegrachi2002@gmail.com>
2025-03-21 14:57:21 +0000

    handle signals

--- src/parsing/intro_to_shell/readline.c
+++ src/parsing/intro_to_shell/readline.c
@@ -5,20 +5,27 @@
 #include <readline/history.h>
 #include <signal.h>
 
-void handler(int sig)
+void handler_SIGINT(int sig)
 {
     (void)sig;
     printf("\n");
-    // rl_replace_line("", 0); // hadi kima glt ma3ndha tachi dawr flhayat
+    rl_replace_line("", 0); // hadi kima glt ma3ndha tachi dawr flhayat
     rl_on_new_line();
     rl_redisplay();
+}
+
+void handler_SIGQUIT(int sig)
+{
+    (void)sig;
+    printf("\b");
 }
 
 int main()
 {
     char *line;
 
-    signal(SIGINT, handler);
+    signal(SIGINT, handler_SIGINT);
+    signal(SIGQUIT, handler_SIGQUIT);
     while (1)
     {
         line = readline("\033[38;2;255;105;180mMinishell > \033[0m");
