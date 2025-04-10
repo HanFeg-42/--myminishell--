3e20c3ed2f91b8beb6695363db5c7ae816594b97
HanFeg-42 <hananefegrachi2002@gmail.com>
2025-04-10 14:49:13 +0100

    I think Quote's parsing is done

--- intro_to_shell/parser.c
+++ intro_to_shell/parser.c
@@ -7,53 +7,51 @@ void	parser(t_token *tok)
 
 void	parse_quotes(t_token *tok)
 {
-	parse_dquote(tok, DQUOTE);
-	parse_dquote(tok, SQUOTE);
-	// int	d_stat;
-	// int	s_stat;
+	int	d_stat;
+	int	s_stat;
 
-	// d_stat = 1;
-	// s_stat = 1;
-	// if (!(*get_parser_check()))
-	// 	return ;
-	// while (tok)
-	// {
-	// 	if (tok->type == DQUOTE && d_stat && s_stat)
-	// 		d_stat = 0;
-	// 	else if (tok->type == DQUOTE && !d_stat)
-	// 		d_stat = 1;
-	// 	if (tok->type == SQUOTE && s_stat && d_stat)
-	// 		s_stat = 0;
-	// 	else if (tok->type == SQUOTE && !s_stat)
-	// 		s_stat = 1;
-	// 	tok = tok->next;
-	// }
-	// if (!d_stat || !s_stat)
-	// {
-	// 	throw_error("unclosed quotes");
-	// 	*get_parser_check() = false;
-	// }
-}
-
-void	parse_dquote(t_token *tok, t_token_type quote)
-{
-	int	q_stat;
-
-	q_stat = 1;
+	d_stat = 1;
+	s_stat = 1;
 	if (!(*get_parser_check()))
 		return ;
 	while (tok)
 	{
-		// printf("hi");
-		if (tok->type == quote && q_stat)
-			q_stat = 0;
-		else if (tok->type == quote && !q_stat)
-			q_stat = 1;
+		if (tok->type == DQUOTE && d_stat && s_stat)
+			d_stat = 0;
+		else if (tok->type == DQUOTE && !d_stat)
+			d_stat = 1;
+		if (tok->type == SQUOTE && s_stat && d_stat)
+			s_stat = 0;
+		else if (tok->type == SQUOTE && !s_stat)
+			s_stat = 1;
 		tok = tok->next;
 	}
-	if (!q_stat)
+	if (!d_stat || !s_stat)
 	{
 		throw_error("unclosed quotes");
 		*get_parser_check() = false;
 	}
 }
+
+// void	parse_dquote(t_token *tok, t_token_type quote)
+// {
+// 	int	q_stat;
+
+// 	q_stat = 1;
+// 	if (!(*get_parser_check()))
+// 		return ;
+// 	while (tok)
+// 	{
+// 		// printf("hi");
+// 		if (tok->type == quote && q_stat)
+// 			q_stat = 0;
+// 		else if (tok->type == quote && !q_stat)
+// 			q_stat = 1;
+// 		tok = tok->next;
+// 	}
+// 	if (!q_stat)
+// 	{
+// 		throw_error("unclosed quotes");
+// 		*get_parser_check() = false;
+// 	}
+// }
